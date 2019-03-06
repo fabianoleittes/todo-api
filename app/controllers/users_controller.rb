6e401ecc183f515456c47c094080c20b4f480935
Fabiano Leite <fabianoleittes@gmail.com>
2019-03-06 20:58:12 -0300

    Create user.
    Add requests test.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  skip_before_action :authorize_request, only: :create
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      json_response(@user, :created)
+    else
+      json_response(@user.errors, :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,12 @@ FactoryBot.define do
   factory :user do
     name { FFaker::Name.name }
     email { FFaker::Internet.email }
-    password_digest { "todo-api" }
+    password { "todo-api" }
+    password_confirmation { "todo-api" }
+  end
+
+  trait :invalid do
+    email {}
+    password {}
   end
 end
