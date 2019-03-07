cd16b08d2d16a5f8b946943f693ee9f9f3950598
Fabiano Leite <fabianoleittes@gmail.com>
2019-03-06 22:07:52 -0300

    Create authorize service.

---
+++ app/services/authorize.rb
@@ -0,0 +1,39 @@
+class Authorize
+  def initialize(headers = {})
+    @headers = headers
+  end
+
+  def self.call(headers)
+    (new(headers).call)[:user]
+  end
+
+  def call
+    {
+      user: user
+    }
+  end
+
+  private
+
+  def user
+    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
+  rescue ActiveRecord::RecordNotFound => e
+    raise(
+      ExceptionHandler::InvalidToken,
+      ("#{Message.invalid_token} #{e.message}")
+    )
+  end
+
+  def decoded_auth_token(adapter = JsonWebTokenAdapter)
+    @decoded_auth_token ||= adapter.decode(http_auth_header)
+  end
+
+  def http_auth_header
+    if headers["Authorization"].present?
+      return headers["Authorization"].split(" ").last
+    end
+    raise(ExceptionHandler::MissingToken, Message.missing_token)
+  end
+
+  attr_reader :headers
+end
