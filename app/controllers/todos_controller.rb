e3865bcceab2cbceb8f2ee75fd18a1349810e33a
Fabiano Leite <fabianoleittes@gmail.com>
2019-03-08 19:00:19 -0300

    Find todo by a specific id.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,12 @@
 class TodosController < ApplicationController
+  before_action :find_todo, only: [:show]
+
   def index
     json_response(current_user.todos.all)
+  end
+
+  def show
+    json_response(@todo)
   end
 
   def create
@@ -16,5 +22,9 @@ class TodosController < ApplicationController
 
   def todo_params
     params.permit(:title)
+  end
+
+  def find_todo
+    @todo = Todo.find(params[:id])
   end
 end
--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Todos API", type: :request do
       before do
         post(
           "/todos",
-          params: todo_params.to_json,
+          params: todo.to_json,
           headers: headers
         )
       end
@@ -14,7 +14,7 @@ RSpec.describe "Todos API", type: :request do
       it "returns a 201 response" do
         expect(response).to have_http_status(:created)
       end
-    end
+   end
 
     context "with invalid data" do
       before do
@@ -59,7 +59,33 @@ RSpec.describe "Todos API", type: :request do
     end
   end
 
-  def todo_params
-    attributes_for(:todo, created_by: user)
+  describe "GET /todos/:id" do
+    before do
+      get(
+        "/todos/#{todo.id}",
+        params: {},
+        headers: headers
+      )
+    end
+    context "when the record exists" do
+      it "returns a response 200" do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "when the record does not exists" do
+      before { get "/todos/#{42}", params: {}, headers: headers }
+      it "returns a response 404" do
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it "returns a found message" do
+        expect(response.body).to match(/Couldn't find Todo/)
+      end
+    end
+  end
+
+  def todo
+    create(:todo, created_by: user)
   end
 end
