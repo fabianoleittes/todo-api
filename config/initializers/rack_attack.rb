b8703f13ca335aee6e4b71414b09bf62ebeb42d5
Fabiano Leite <fabianoleittes@gmail.com>
2019-03-29 17:41:30 -0300

    Add rack attack.

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,11 @@
+class Rack::Attack
+  # Allow all local traffic
+  safelist("allow-localhost") do |req|
+    "127.0.0.1" == req.ip || "::1" == req.ip
+  end
+
+  # Allow an IP address to make 10 requests every 10 seconds
+  throttle("req/ip", limit: 5, period: 5) do |req|
+    req.ip
+  end
+end
